a0f58867b3c89d0bbf28bcc74423773353ee27f1
1kakadu1 <1kakady1@gmail.com>
2022-06-15 18:06:25 +0300

    change connect db

---
+++ prisma/migrations/20220615145559_init/migration.sql
@@ -0,0 +1,310 @@
+-- CreateTable
+CREATE TABLE "CarModel" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "CarModel_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Seasons" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Seasons_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Conditions" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Conditions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Manufacturers" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Manufacturers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SpeedIndex" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "SpeedIndex_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FuelEfficiency" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "FuelEfficiency_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "GripSurfaces" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "GripSurfaces_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "preview" TEXT NOT NULL DEFAULT E'',
+    "description" TEXT NOT NULL DEFAULT E'',
+    "isProduct" BOOLEAN NOT NULL DEFAULT false,
+    "isServices" BOOLEAN NOT NULL DEFAULT false,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "updatedAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "avatar" TEXT NOT NULL DEFAULT E'',
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+    "updatedAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Comments" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "comment" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+    "productId" TEXT NOT NULL,
+
+    CONSTRAINT "Comments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CommentsNews" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "comment" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+    "newsId" TEXT NOT NULL,
+
+    CONSTRAINT "CommentsNews_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductCategory" (
+    "categoryId" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+
+    CONSTRAINT "ProductCategory_pkey" PRIMARY KEY ("categoryId","productId")
+);
+
+-- CreateTable
+CREATE TABLE "ServicesCategory" (
+    "categoryId" TEXT NOT NULL,
+    "serviceId" TEXT NOT NULL,
+
+    CONSTRAINT "ServicesCategory_pkey" PRIMARY KEY ("categoryId","serviceId")
+);
+
+-- CreateTable
+CREATE TABLE "NewsCategory" (
+    "categoryId" TEXT NOT NULL,
+    "newsId" TEXT NOT NULL,
+
+    CONSTRAINT "NewsCategory_pkey" PRIMARY KEY ("categoryId","newsId")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "code" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "width" INTEGER NOT NULL,
+    "modeCarlId" INTEGER NOT NULL DEFAULT 1,
+    "userId" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "rating" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "conditionId" INTEGER NOT NULL DEFAULT 1,
+    "seasonId" INTEGER NOT NULL DEFAULT 1,
+    "manufacturersId" INTEGER NOT NULL DEFAULT 1,
+    "speedIndexId" INTEGER NOT NULL DEFAULT 1,
+    "fuelEfficiencyId" INTEGER NOT NULL DEFAULT 1,
+    "gripSurfacesId" INTEGER NOT NULL DEFAULT 1,
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "preview" TEXT NOT NULL DEFAULT E'',
+    "slug" TEXT NOT NULL,
+    "description" TEXT NOT NULL DEFAULT E'',
+    "loadIndex" INTEGER NOT NULL DEFAULT 0,
+    "externalNoiseLevel" INTEGER NOT NULL DEFAULT 0,
+    "completeSet" BOOLEAN NOT NULL DEFAULT false,
+    "isStock" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Services" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL DEFAULT E'',
+    "preview" TEXT NOT NULL DEFAULT E'',
+    "previewFull" TEXT NOT NULL DEFAULT E'',
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "userId" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "workDesc" TEXT NOT NULL DEFAULT E'Работаем с 9.00 - 20.00 без выходных',
+    "slug" TEXT NOT NULL,
+
+    CONSTRAINT "Services_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "News" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL DEFAULT E'',
+    "preview" TEXT NOT NULL DEFAULT E'',
+    "previewFull" TEXT NOT NULL DEFAULT E'',
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "isStock" BOOLEAN NOT NULL DEFAULT false,
+    "userId" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "validUntil" TIMESTAMP(3),
+    "tags" TEXT,
+
+    CONSTRAINT "News_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Orders" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "products" TEXT NOT NULL,
+
+    CONSTRAINT "Orders_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_slug_key" ON "Category"("name", "slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_id_post_id_unique" ON "Comments"("userId", "productId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_id_newsId_unique" ON "CommentsNews"("userId", "newsId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_id_name_code_key" ON "Product"("id", "name", "code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Services_id_title_key" ON "Services"("id", "title");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "News_id_title_key" ON "News"("id", "title");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Orders_id_key" ON "Orders"("id");
+
+-- AddForeignKey
+ALTER TABLE "Comments" ADD CONSTRAINT "Comments_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comments" ADD CONSTRAINT "Comments_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CommentsNews" ADD CONSTRAINT "CommentsNews_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CommentsNews" ADD CONSTRAINT "CommentsNews_newsId_fkey" FOREIGN KEY ("newsId") REFERENCES "News"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductCategory" ADD CONSTRAINT "ProductCategory_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductCategory" ADD CONSTRAINT "ProductCategory_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ServicesCategory" ADD CONSTRAINT "ServicesCategory_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ServicesCategory" ADD CONSTRAINT "ServicesCategory_serviceId_fkey" FOREIGN KEY ("serviceId") REFERENCES "Services"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "NewsCategory" ADD CONSTRAINT "NewsCategory_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "NewsCategory" ADD CONSTRAINT "NewsCategory_newsId_fkey" FOREIGN KEY ("newsId") REFERENCES "News"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_modeCarlId_fkey" FOREIGN KEY ("modeCarlId") REFERENCES "CarModel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_seasonId_fkey" FOREIGN KEY ("seasonId") REFERENCES "Seasons"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_conditionId_fkey" FOREIGN KEY ("conditionId") REFERENCES "Conditions"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_manufacturersId_fkey" FOREIGN KEY ("manufacturersId") REFERENCES "Manufacturers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_speedIndexId_fkey" FOREIGN KEY ("speedIndexId") REFERENCES "SpeedIndex"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_fuelEfficiencyId_fkey" FOREIGN KEY ("fuelEfficiencyId") REFERENCES "FuelEfficiency"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_gripSurfacesId_fkey" FOREIGN KEY ("gripSurfacesId") REFERENCES "GripSurfaces"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Services" ADD CONSTRAINT "Services_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "News" ADD CONSTRAINT "News_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Orders" ADD CONSTRAINT "Orders_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
